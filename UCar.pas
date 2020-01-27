1cf41fa087bb3991104dbf2c4795b452bdf1200c
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-28 05:18:22 +1400

    car Create

---
+++ UCar.pas
@@ -0,0 +1,21 @@
+﻿unit UCar;
+
+interface
+
+type
+
+  TCar = class
+    X, V: real;
+    P: integer;
+    road: TRoad;
+    constructor Create(road0: TRoad; p0: integer; v0: real);
+  end;
+
+implementation
+
+constructor TCar.Create(road0: TRoad; p0: integer; v0: real);
+begin
+  road:=road0; P:=p0; V:=v0;
+end;
+
+end.
