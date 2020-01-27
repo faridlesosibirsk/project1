baa7f2b61542cf45c47d02a9ac4bccae74e9aefd
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-28 05:09:37 +1400

    road

--- Unit1.pas
+++ Unit1.pas
@@ -2,12 +2,18 @@
 
 interface
 
-uses System, System.Drawing, System.Windows.Forms;
+uses 
+URoad,
+System, System.Drawing, System.Windows.Forms;
 
 type
   Form1 = class(Form)
+    procedure buttonRoad_Click(sender: Object; e: EventArgs);
   {$region FormDesigner}
   private
+    road: TRoad;
+    {$resource Unit1.Form1.resources}
+    buttonRoad: Button;
     {$include Unit1.Form1.inc}
   {$endregion FormDesigner}
   public
@@ -18,5 +24,12 @@ type
   end;
 
 implementation
+
+procedure Form1.buttonRoad_Click(sender: Object; e: EventArgs);
+begin
+  road:=TRoad.Create;
+  road.Length:=60;
+  road.Width:=3;
+end;
 
 end.
